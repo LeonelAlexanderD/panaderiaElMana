1fd1875eaef99098d02ad679cfb524daa8cb5dbc
Leonel Alexander Diaz <chaffs25@gmail.com>
2024-11-04 01:38:15 -0300

    inicio de session

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            return render(request,'usuarios/login.html',{"msj":"Datos incorrectos"})
+    return render(request,'usuarios/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('inicio')
